4e32ebf0a270f3abadd0f056a771aa12f043633d
alex-samusenko <alex.samusenko@gmail.com>
2022-10-13 16:18:48 +0300

    move scanner to try block

--- src/main/java/HomeWork.java
+++ src/main/java/HomeWork.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class HomeWork {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please, enter your name");
-        String name = sc.nextLine();
-        String encodedName = Base64.getEncoder().encodeToString(name.getBytes());
-        System.out.println("Hello " + encodedName);
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Please, enter your name");
+            String name = sc.nextLine();
+            String encodedName = Base64.getEncoder().encodeToString(name.getBytes());
+            System.out.println("Program result: " + encodedName);
+        }
     }
 }
